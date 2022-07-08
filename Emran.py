738c66cff9c819eaf5da1ac9db58c48683d34b90
Mohammad Bazmara <mohamad.bazmara@gmail.com>
2022-07-08 16:32:26 +0430

    Revert "edite3 by amjad"
    This reverts commit 39d906cc

--- Emran.py
+++ Emran.py
@@ -3,11 +3,7 @@ from getpass import getpass
 username = input("Enter Your Username: ")
 password = getpass("Enter Your Password: ")
 
-
-if username == "amjad" and password == "maktab78":
-   pass
-if username == "asghar" and password == "maktab78":
-
+if username == "pouryaaaaaaaaaaaaaaaa" and password == "maktab78":
     print("Login Successfully.")
 else:
     print("Username or Password Wrong.")
