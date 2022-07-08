1638943a4257e7502d4f6965556664ae293f4422
amjad ahmadi <amjadahmadi883@gmail.com>
2022-07-08 16:39:13 +0430

    edite 5 by amjad

--- Emran.py
+++ Emran.py
@@ -3,7 +3,7 @@ from getpass import getpass
 username = input("Enter Your Username: ")
 password = getpass("Enter Your Password: ")
 
-if username == "pouryaaaaaaaaaaaaaaaa" and password == "pourya is very crazy":
+if username == "pouryaaaaaaaaaaaaaaaa" and password == "pourya is very good":
     print("Login Successfully.")
 else:
     print("Username or Password Wrong.")
